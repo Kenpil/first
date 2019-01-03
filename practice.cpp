77ea153e73eda757fe089ab1b4ed243fc2476d36
Kenpil <hdmiken@gmail.com>
2019-01-03 13:07:30 +0900

    Create practice.cpp

---
+++ practice.cpp
@@ -0,0 +1,9 @@
+{
+	"folders": [],
+	"settings": {}
+    "runner.languageMap": {
+		"ruby": "C:/Robots/VSCode/VSCodeRunner/rubyrun.bat"
+        "c": "C:/Robots/VSCode/VSCodeRunner/crun_gcc.bat"    // gccの場合
+         "cpp": "C:/Robots/VSCode/VSCodeRunner/cpprun_gcc.bat"    // gccの場合
+    }
+}
